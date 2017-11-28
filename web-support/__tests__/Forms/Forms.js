b11dcc9bbb41af3c27384edb2f0c898109e1e381
Gregor71 <millergregor@gmail.com>
2017-11-28 07:37:58 +0000

    assume all guides built via jest

--- web-support/__tests__/Forms/Forms.js
+++ web-support/__tests__/Forms/Forms.js
@@ -2,13 +2,12 @@ import path from 'path';
 import { runTests, buildGuide } from '../../sampleParser';
 import sections from '../../samples';
 
-const PROJECT_ROOT = path.resolve(__dirname, '../../..');
-const ROOT_PATH = path.join(PROJECT_ROOT, 'src');
-const GUIDE_DIR = path.join(PROJECT_ROOT, 'web-support', 'styleguide');
-const EXAMPLES_DIR = path.join(GUIDE_DIR, 'examples');
+const EXAMPLES_DIR = path.resolve(
+  __dirname,
+  '../../../web-support/styleguide/examples'
+);
 
-const opts = { dev: true };
-Object.assign(opts, { PROJECT_ROOT, ROOT_PATH, GUIDE_DIR, EXAMPLES_DIR });
+const opts = { dev: true, EXAMPLES_DIR };
 
 runTests(sections.Forms);
 buildGuide({ Forms: sections.Forms }, opts);
